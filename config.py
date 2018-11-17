f38acd121814dfb74c8e34fe292c53d4bcc4192c
lorna nashipae <kimanilorna@yahoo.com>
2018-11-18 01:31:58 +0300

    This commit is to download and import flask log into various files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorna:0724276722@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorna:0724276722@localhost/pitches'
 
     pass
 class ProdConfig(Config):
