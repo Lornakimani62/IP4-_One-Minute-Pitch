11e095b39b5dd6724c494e65c7ca8eb7087ccd87
lorna nashipae <kimanilorna@yahoo.com>
2018-11-18 08:51:00 +0300

    This commit is to add configurations to the database uri

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorna:0724276722@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
 
     pass
 class ProdConfig(Config):
