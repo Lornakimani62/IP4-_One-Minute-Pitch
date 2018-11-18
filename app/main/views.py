c6e1cbf31e978dc2853042262b70c26acc59a205
lorna nashipae <kimanilorna@yahoo.com>
2018-11-18 14:22:45 +0300

    This commit is to add the first view function for the homepage

--- app/main/views.py
+++ app/main/views.py
@@ -4,16 +4,11 @@ from . import main
 from .. import db
 from ..models import Pitch,Comments,User,Upvote,Downvote
 
-# @main.route('/index', methods=['GET', 'POST'])
-# def home():
-#     form = PostForm()
-#     if form.validate_on_submit():
-#         post = Pitch(body=form.post.data, author=current_user, category=form.category.data)
-#         db.session.add(post)
-#         db.session.commit()
-#         flash('Your post is now live!')
-#         return redirect(url_for('main.index'))
-
-#     posts = Pitch.retrieve_posts(id).all()
-
-#     return render_template("posts.html", title='Home Page', form=form,posts=posts)
+@main.route('/')
+def home():
+    posts = Pitch.query.all()
+    categories = db.session.query(Pitch.category).distinct().all()
+    defaultcategories = [('pickup lines',),('interview pitch',),('product pitch',),('business pitch',)]
+    categories = set(categories+defaultcategories)
+    print(categories)
+    return render_template('post.html', title = title,posts=posts,categories=categories)
