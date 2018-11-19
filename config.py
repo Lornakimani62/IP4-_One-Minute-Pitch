2514215c2521107bb0c35cd9862a575fd6ee8087
lorna nashipae <kimanilorna@yahoo.com>
2018-11-19 10:01:22 +0300

    This commit is to send an email to new users after they register

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
     pass
 class ProdConfig(Config):
     pass
--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export SECRET_KEY='secret key'
+export MAIL_USERNAME='ideahub96@gmail.com'
+export MAIL_PASSWORD='0724276722'
+
 python3.6 manage.py server
 
