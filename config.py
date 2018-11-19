0241bf105fb333de100efa344f663109a10a5dd8
lorna nashipae <kimanilorna@yahoo.com>
2018-11-19 03:10:05 +0300

    This commit is to add the secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorna:0724276722@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     pass
 class ProdConfig(Config):
     pass
